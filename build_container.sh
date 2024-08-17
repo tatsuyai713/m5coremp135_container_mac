60f623c726aa050055d825b8327b50cdf2dc3b91
Tatsuya Ishikawa <tatsuyai713@gmail.com>
2024-08-17 14:17:38 +0900

    Fix platform

--- build_container.sh
+++ build_container.sh
@@ -11,13 +11,9 @@ if [ "$(docker image ls -q ${NAME_IMAGE})" ]; then
 	exit
 fi
 
-if [ ! "$(docker image ls -q ${BASE_IMAGE})" ]; then
-	docker import ./m5_rootfs_20240515.tar.gz m5coremp135:20240515
-fi
-
 echo "Build Container"
 
-docker build --platform linux/arm/v7 --file=./Dockerfile -t $NAME_IMAGE . --build-arg UID=$(id -u) --build-arg GID=$(id -u) --build-arg UNAME=$USER --build-arg SETLOCALE='JP'
+docker build --file=./Dockerfile -t $NAME_IMAGE . --build-arg UID=$(id -u) --build-arg GID=$(id -u) --build-arg UNAME=$USER --build-arg SETLOCALE='JP'
 
 
 echo "_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/"
